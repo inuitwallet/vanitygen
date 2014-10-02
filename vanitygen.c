e444e4b3e2459c27cb7f22131f7773431d87881d
MJ2P <mj2p.sg@gmail.com>
2014-10-02 11:12:29 +0100

    duplicated main loop code to allow for three loops. this allows for system information to be gathered on stdout for display to the end user (time remaining etc).

--- vanitygen.c
+++ vanitygen.c
@@ -239,6 +239,211 @@ out:
 	return NULL;
 }
 
+/* Duplicate thread loop with controlled loop to gather system info rather than find the address */
+
+void *
+vg_thread_system_info(void *arg)
+{
+	unsigned char hash_buf[128];
+	unsigned char *eckey_buf;
+	unsigned char hash1[32];
+
+	int i, c, len, output_interval, n;
+	int hash_len;
+
+	const BN_ULONG rekey_max = 10000000;
+	BN_ULONG npoints, rekey_at, nbatch;
+
+	vg_context_t *vcp = (vg_context_t *) arg;
+	EC_KEY *pkey = NULL;
+	const EC_GROUP *pgroup;
+	const EC_POINT *pgen;
+	const int ptarraysize = 256;
+	EC_POINT *ppnt[ptarraysize];
+	EC_POINT *pbatchinc;
+
+	vg_test_func_t test_func = vcp->vc_test;
+	vg_exec_context_t ctx;
+	vg_exec_context_t *vxcp;
+
+	struct timeval tvstart;
+
+
+	memset(&ctx, 0, sizeof(ctx));
+	vxcp = &ctx;
+
+	vg_exec_context_init(vcp, &ctx);
+
+	pkey = vxcp->vxc_key;
+	pgroup = EC_KEY_get0_group(pkey);
+	pgen = EC_GROUP_get0_generator(pgroup);
+
+	for (i = 0; i < ptarraysize; i++) {
+		ppnt[i] = EC_POINT_new(pgroup);
+		if (!ppnt[i]) {
+			fprintf(stderr, "ERROR: out of memory?\n");
+			exit(1);
+		}
+	}
+	pbatchinc = EC_POINT_new(pgroup);
+	if (!pbatchinc) {
+		fprintf(stderr, "ERROR: out of memory?\n");
+		exit(1);
+	}
+
+	BN_set_word(&vxcp->vxc_bntmp, ptarraysize);
+	EC_POINT_mul(pgroup, pbatchinc, &vxcp->vxc_bntmp, NULL, NULL,
+		     vxcp->vxc_bnctx);
+	EC_POINT_make_affine(pgroup, pbatchinc, vxcp->vxc_bnctx);
+
+	npoints = 0;
+	rekey_at = 0;
+	nbatch = 0;
+	vxcp->vxc_key = pkey;
+	vxcp->vxc_binres[0] = vcp->vc_addrtype;
+	c = 0;
+	output_interval = 1000;
+	gettimeofday(&tvstart, NULL);
+
+	if (vcp->vc_format == VCF_SCRIPT) {
+		hash_buf[ 0] = 0x51;  // OP_1
+		hash_buf[ 1] = 0x41;  // pubkey length
+		// gap for pubkey
+		hash_buf[67] = 0x51;  // OP_1
+		hash_buf[68] = 0xae;  // OP_CHECKMULTISIG
+		eckey_buf = hash_buf + 2;
+		hash_len = 69;
+
+	} else {
+		eckey_buf = hash_buf;
+		hash_len = 65;
+	}
+
+	n = 0;
+	
+	while (n < 3) {
+		if (++npoints >= rekey_at) {
+			vg_exec_context_upgrade_lock(vxcp);
+			/* Generate a new random private key */
+			EC_KEY_generate_key(pkey);
+			npoints = 0;
+
+			/* Determine rekey interval */
+			EC_GROUP_get_order(pgroup, &vxcp->vxc_bntmp,
+					   vxcp->vxc_bnctx);
+			BN_sub(&vxcp->vxc_bntmp2,
+			       &vxcp->vxc_bntmp,
+			       EC_KEY_get0_private_key(pkey));
+			rekey_at = BN_get_word(&vxcp->vxc_bntmp2);
+			if ((rekey_at == BN_MASK2) || (rekey_at > rekey_max))
+				rekey_at = rekey_max;
+			assert(rekey_at > 0);
+
+			EC_POINT_copy(ppnt[0], EC_KEY_get0_public_key(pkey));
+			vg_exec_context_downgrade_lock(vxcp);
+
+			npoints++;
+			vxcp->vxc_delta = 0;
+
+			if (vcp->vc_pubkey_base)
+				EC_POINT_add(pgroup,
+					     ppnt[0],
+					     ppnt[0],
+					     vcp->vc_pubkey_base,
+					     vxcp->vxc_bnctx);
+
+			for (nbatch = 1;
+			     (nbatch < ptarraysize) && (npoints < rekey_at);
+			     nbatch++, npoints++) {
+				EC_POINT_add(pgroup,
+					     ppnt[nbatch],
+					     ppnt[nbatch-1],
+					     pgen, vxcp->vxc_bnctx);
+			}
+
+		} else {
+			/*
+			 * Common case
+			 *
+			 * EC_POINT_add() can skip a few multiplies if
+			 * one or both inputs are affine (Z_is_one).
+			 * This is the case for every point in ppnt, as
+			 * well as pbatchinc.
+			 */
+			assert(nbatch == ptarraysize);
+			for (nbatch = 0;
+			     (nbatch < ptarraysize) && (npoints < rekey_at);
+			     nbatch++, npoints++) {
+				EC_POINT_add(pgroup,
+					     ppnt[nbatch],
+					     ppnt[nbatch],
+					     pbatchinc,
+					     vxcp->vxc_bnctx);
+			}
+		}
+
+		/*
+		 * The single most expensive operation performed in this
+		 * loop is modular inversion of ppnt->Z.  There is an
+		 * algorithm implemented in OpenSSL to do batched inversion
+		 * that only does one actual BN_mod_inverse(), and saves
+		 * a _lot_ of time.
+		 *
+		 * To take advantage of this, we batch up a few points,
+		 * and feed them to EC_POINTs_make_affine() below.
+		 */
+
+		EC_POINTs_make_affine(pgroup, nbatch, ppnt, vxcp->vxc_bnctx);
+
+		for (i = 0; i < nbatch; i++, vxcp->vxc_delta++) {
+			/* Hash the public key */
+			len = EC_POINT_point2oct(pgroup, ppnt[i],
+						 POINT_CONVERSION_UNCOMPRESSED,
+						 eckey_buf,
+						 65,
+						 vxcp->vxc_bnctx);
+			assert(len == 65);
+
+			SHA256(hash_buf, hash_len, hash1);
+			RIPEMD160(hash1, sizeof(hash1), &vxcp->vxc_binres[1]);
+
+			switch (test_func(vxcp)) {
+			case 1:
+				npoints = 0;
+				rekey_at = 0;
+				i = nbatch;
+				break;
+			case 2:
+				goto out;
+			default:
+				break;
+			}
+		}
+
+		c += i;
+		if (c >= output_interval) {
+			output_interval = vg_output_timing(vcp, c, &tvstart);
+			if (output_interval > 250000)
+				output_interval = 250000;
+			c = 0;
+			n++;
+		}
+
+		vg_exec_context_yield(vxcp);
+	}
+
+out:
+	vg_exec_context_del(&ctx);
+	vg_context_thread_exit(vcp);
+
+	for (i = 0; i < ptarraysize; i++)
+		if (ppnt[i])
+			EC_POINT_free(ppnt[i]);
+	if (pbatchinc)
+		EC_POINT_free(pbatchinc);
+	return NULL;
+}
+
 
 #if !defined(_WIN32)
 int
@@ -314,6 +519,7 @@ usage(const char *name)
 "-N            Generate namecoin address\n"
 "-T            Generate bitcoin testnet address\n"
 "-X <version>  Generate address with the given version\n"
+"              Alternatively, specifiy <addressversion#privatekeyversion> when they differ\n"
 "-F <format>   Generate address with the given format (pubkey or script)\n"
 "-P <pubkey>   Specify base public key for piecewise key generation\n"
 "-e            Encrypt private keys, prompt for password\n"
@@ -615,8 +821,10 @@ main(int argc, char **argv)
 		fprintf(stderr,
 			"Regular expressions: %ld\n", vcp->vc_npatterns);
 
-	if (simulate)
+	if (simulate) {
+		vg_thread_system_info(vcp);
 		return 0;
+	}
 
 	if (!start_threads(vcp, nthreads))
 		return 1;
